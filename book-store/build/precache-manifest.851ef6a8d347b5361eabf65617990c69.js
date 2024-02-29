cb29d1e00dc2210cc65d6b389d73b7a60dc7eabc
Renato Caceres <crcaceres05@gmail.com>
2024-02-28 20:21:52 -0600

    Inicio de Proyecto DevOps

---
+++ book-store/build/precache-manifest.851ef6a8d347b5361eabf65617990c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adb46bc42e6646e75a2c7f1f1ed4f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe5bdd5098dd491277c",
+    "url": "/static/css/main.a19d3ecf.chunk.css"
+  },
+  {
+    "revision": "9a8d09819de6ba55f2ae",
+    "url": "/static/js/2.d2cde26a.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "afe5bdd5098dd491277c",
+    "url": "/static/js/main.556ba792.chunk.js"
+  },
+  {
+    "revision": "49b1760ced1044a52545",
+    "url": "/static/js/runtime~main.e2e3066b.js"
+  }
+]);
